17a3b72b60286b97c68e87035732e3d91a37c785
mookie34 <santiagomazo34@gmail.com>
2025-09-19 22:13:39 -0500

    Add methods order items

--- routes/orderItems.js
+++ routes/orderItems.js
@@ -8,5 +8,11 @@ router.post('/', orderItemsController.addOrderItem);
 router.get('/', orderItemsController.getAllOrderItems);
 // Obtener items de orden por ID de orden
 router.get('/order/:orderId', orderItemsController.getOrderItemByOrderId);
+// Eliminar todos items de orden por ID de orden
+router.delete('/order/:orderId', orderItemsController.deleteAllItemsInOrder);
+// Eliminar un item de orden por ID de orden y ID de producto
+router.delete('/order/:orderId/product/:productId', orderItemsController.deleteItemInOrderByIdProduct);
+// Actualizar cantidad o precio en un item de orden
+router.patch('/order/:orderId/product/:productId', orderItemsController.updateQuantityOrPriceInOrderItem);
 
 module.exports = router;
